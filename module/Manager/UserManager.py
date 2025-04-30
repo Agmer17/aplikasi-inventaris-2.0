fabca756453d8cf2f8ca42ee14479d5029ac26ee
Rizky Agmer <agmerramadhan@gmail.com>
2025-04-30 18:07:05 +0700

    getFullData

--- module/Manager/UserManager.py
+++ module/Manager/UserManager.py
@@ -197,6 +197,14 @@ class UserManager:
 
         # Simpan perubahan ke file JSON
         self.changeData()
+        
+    def getAllData(self) -> dict[str:object] : 
+        temp = {}
+        
+        for key, val in self.items.items() :
+            temp.update({key : val.getFullData()})
+        
+        return temp
 
 
 
